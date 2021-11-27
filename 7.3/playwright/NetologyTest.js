0c88b0ed9227385a2d4922e18d1961eacd7baf2e
“Yuri <yura.kopshev@gmail.com>
2021-11-27 17:05:44 +0400

    changed NetologyTest.js

--- 7.3/playwright/NetologyTest.js
+++ 7.3/playwright/NetologyTest.js
@@ -2,20 +2,20 @@
 
 const user = require('../playwright/user');
 const { chromium } = require('playwright');
-const { test, expect } = require("@playwright/test");
+const { expect } = require("@playwright/test");
 
 //positive test
 (async () => {
   const browser = await chromium.launch({
-    //headless: false,
+    headless: false,
     slowMo: 5000,
   });
   const page = await browser.newPage();
   await page.goto("https://netology.ru");
   await page.click('.shared-components-Header-header-module__login--JN4tR');
-  
-  await page.fill('[placeholder="Email"]',user.login);
-  await page.fill('[placeholder="Пароль"]',user.pass);
+
+  await page.fill('[placeholder="Email"]', user.login);
+  await page.fill('[placeholder="Пароль"]', user.pass);
   await page.click('text=Войти');
   await expect(page).toHaveURL("https://netology.ru/profile");
   await browser.close();
@@ -24,7 +24,7 @@ const { test, expect } = require("@playwright/test");
 //negative test
 (async () => {
   const browser = await chromium.launch({
-    //headless: false,
+    headless: false,
     slowMo: 5000,
   });
 
@@ -35,7 +35,7 @@ const { test, expect } = require("@playwright/test");
   await page.fill('[placeholder="Пароль"]', '12345');
   await page.click('text=Войти');
   await expect(
-		page.locator("text=Вы ввели неправильно логин или пароль")
-	).toBeVisible();
+    page.locator("text=Вы ввели неправильно логин или пароль")
+  ).toBeVisible();
   await browser.close();
 })();
